ab044907ef07b84dcc3bab9115a9efa29aec8e5a
David Chaves <davch09@hotmail.com>
2025-06-28 11:09:52 -0500

    funcionalidad de los botones con JS y descripcion README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tripleten web_project_around
+
+Alrededor de los Estados unidos
+
+Pagina interactiva para el usuario, siguiendo instrucciones de acuerdo al brief entregado en Figma, con el fin de realizar un diseño responsivo que se ajuste a diferentes tamaños de pantallas y estas luzcan con una apariencia agradable para el lector. Para el desarrollo del proyecto, se implementa el uso de JavaScript, metodologia BEM, @media-queries, popups, formularios funcionales, botones interactivos y se enlaza con github pages.
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,42 @@
-console.log("JS FUNCIONANDO");
+const editButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closeButton = document.querySelector(".popup__close-button");
+const profileName = document.querySelector(".profile__name");
+const profileRole = document.querySelector(".profile__role");
+const nameInput = document.querySelector(".popup__input_profile_name");
+const roleInput = document.querySelector(".popup__input_profile_role");
+const formElement = document.querySelector(".popup__form");
+const likeButtons = document.querySelectorAll(".card__like-button");
+
+function openPopup() {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  roleInput.value = profileRole.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  const newName = nameInput.value;
+  const newRole = roleInput.value;
+
+  profileName.textContent = newName;
+  profileRole.textContent = newRole;
+
+  closePopup();
+}
+
+function toggleLike(evt) {
+  evt.target.classList.toggle("card__like-button_active");
+}
+
+editButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
+formElement.addEventListener("submit", handleProfileFormSubmit);
+likeButtons.forEach(function (button) {
+  button.addEventListener("click", toggleLike);
+});
